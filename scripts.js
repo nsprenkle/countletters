7ea1c34e2fe059bef1cf02fc5665e23d023989bf
Benny Mei <meibenny@gmail.com>
2015-05-22 01:23:54 +0000

    first commit

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function countLetters(){
+  var text = location.search.match(/\?data=(.*)/)[1];
+  text = text.replace(/\+/g, '');
+  var char_count = new Object();
+  var text_length = text.length;
+  for(var i = 0; i < text_length; i++){
+    char_count[text.charAt(i)] = 0;
+  }
+  for(var i = 0; i < text_length; i++){
+    char_count[text.charAt(i)] += 1;
+  }
+  text = "";
+  for(var eachProperty in char_count){
+    text += eachProperty + " " + char_count[eachProperty] + "<br>";
+  }
+  document.getElementById("results").innerHTML += text;
+}
+
