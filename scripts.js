d8e0d3455e3cfd2c16181c434cd3695b8b09d0b4
Benny Mei <meibenny@gmail.com>
2015-08-21 22:47:52 +0000

    lowercase input text

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 function countLetters(){
   var text = location.search.match(/\?data=(.*)/)[1];
   text = text.replace(/\+/g, '');
+  text = text.toLowerCase();
   var char_count = new Object();
   var text_length = text.length;
   for(var i = 0; i < text_length; i++){
