e15a1b8b7b2769aa939d9251ee9c386b37514249
nsprenkle <nathan.sprenkle@gmail.com>
2016-03-13 14:25:55 -0400

    Restyled page adding color and form styling. Autochecks check box on page reload if previously selected.

--- scripts.js
+++ scripts.js
@@ -1,34 +1,52 @@
+/* get input from query string */
 function findParameters(parameter){
-  //cut out the question mark at the beginning of the query string
-  var queryString = window.location.search.substring(1);
-  var variables = queryString.split("&");
-  for(var i = 0; i < variables.length; i++){
-    var pair = variables[i].split("=");
-    if(pair[0] == parameter){
-      return pair[1];
+    //cut out the question mark at the beginning of the query string
+    var queryString = window.location.search.substring(1),
+        variables = queryString.split("&"),
+        pair = [];
+
+    for(var i = 0; i < variables.length; i++){
+        pair = variables[i].split("=");
+        if(pair[0] == parameter){
+            return pair[1];
+        }
     }
-  }
-  return (false);
+
+    return (false);
 }
 
 function countLetters(){
-  var textIn = findParameters("data");
-  textIn = textIn.replace(/\+/g, '');
-  if(window.location.href.indexOf("caps=1") == -1){
-    textIn = textIn.toLowerCase();
-  }
-  textIn = decodeURIComponent(textIn)
-  var char_count = new Object();
-  var textIn_length = textIn.length;
-  for(var i = 0; i < textIn_length; i++){
-    char_count[textIn.charAt(i)] = 0;
-  }
-  for(var i = 0; i < textIn_length; i++){
-    char_count[textIn.charAt(i)] += 1;
-  }
-  textIn = "";
-  for(var eachProperty in char_count){
-    textIn += eachProperty + " " + char_count[eachProperty] + "<br>";
-  }
-  document.getElementById("results").innerHTML += textIn;
+    var textIn = "",
+        char_count = new Object(),
+        textIn_length = 0;
+
+    // get input from query string
+    textIn = findParameters("data");
+    document.getElementById("inputbox").innerHTML = textIn;
+    textIn = textIn.replace(/\+/g, '');
+
+    if(window.location.href.indexOf("caps=1") == -1){
+        textIn = textIn.toLowerCase();
+    } else {
+        document.getElementById("caps-checkbox").checked = true;
+    }
+
+    textIn = decodeURIComponent(textIn);
+    textIn_length = textIn.length;
+
+    for(var i = 0; i < textIn_length; i++){
+        char_count[textIn.charAt(i)] = 0;
+    }
+
+    for(var i = 0; i < textIn_length; i++){
+        char_count[textIn.charAt(i)] += 1;
+    }
+    textIn = "";
+
+    for(var eachProperty in char_count){
+        textIn += eachProperty + " " + char_count[eachProperty] + "<br>";
+    }
+
+    document.getElementById("results").innerHTML += textIn;
+
 }
