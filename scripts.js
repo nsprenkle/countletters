3eeb651b6530f79c3ff3f82dc3b875a9c589d1a3
nsprenkle <nathan.sprenkle@gmail.com>
2019-04-07 17:42:43 -0400

    Format scripts.js using standard

--- scripts.js
+++ scripts.js
@@ -1,55 +1,60 @@
 /* get input from query string */
-function findParameters(parameter) {
-  //cut out the question mark at the beginning of the query string
-  var queryString = window.location.search.substring(1),
-    variables = queryString.split("&"),
-    pair = [];
+function findParameters (parameter) {
+  // cut out the question mark at the beginning of the query string
+  var queryString = window.location.search.substring(1)
+
+  var variables = queryString.split('&')
+
+  var pair = []
 
   for (var i = 0; i < variables.length; i++) {
-    pair = variables[i].split("=");
-    if (pair[0] == parameter) {
-      return pair[1];
+    pair = variables[i].split('=')
+    if (pair[0] === parameter) {
+      return pair[1]
     }
   }
 
-  return false;
+  return false
 }
 
-function countLetters() {
-  var textIn = "",
-    char_count = new Object(),
-    textIn_length = 0;
+function countLetters () { // eslint-disable-line no-unused-vars
+  var textIn = ''
+
+  var charCount = {}
+
+  var textInLength = 0
 
   // get input from query string
-  textIn = findParameters("data") || "";
-  document.getElementById("inputbox").innerHTML = textIn;
-  textIn = textIn.replace(/\+/g, "");
+  textIn = findParameters('data') || ''
+  document.getElementById('inputbox').innerHTML = textIn
+  textIn = textIn.replace(/\+/g, '')
 
-  if (window.location.href.indexOf("caps=1") == -1) {
-    textIn = textIn.toLowerCase();
+  if (window.location.href.indexOf('caps=1') === -1) {
+    textIn = textIn.toLowerCase()
   } else {
-    document.getElementById("caps-checkbox").checked = true;
+    document.getElementById('caps-checkbox').checked = true
   }
 
-  textIn = decodeURIComponent(textIn);
-  textIn_length = textIn.length;
+  textIn = decodeURIComponent(textIn)
+  textInLength = textIn.length
 
-  for (var i = 0; i < textIn_length; i++) {
-    char_count[textIn.charAt(i)] = 0;
+  for (var i = 0; i < textInLength; i++) {
+    charCount[textIn.charAt(i)] = 0
   }
 
-  for (var i = 0; i < textIn_length; i++) {
-    char_count[textIn.charAt(i)] += 1;
+  for (var i = 0; i < textInLength; i++) { // eslint-disable-line no-redeclare
+    charCount[textIn.charAt(i)] += 1
   }
-  textIn = "";
 
-  for (var eachProperty in char_count) {
+  textIn = ''
+
+  for (var eachProperty in charCount) {
     textIn +=
       eachProperty +
       "<span class='multiplier'>(" +
-      char_count[eachProperty] +
-      "x)</span><br>";
+      charCount[eachProperty] +
+      'x)</span><br>'
   }
 
-  document.getElementById("results").innerHTML += textIn;
+  document.getElementById('results').innerHTML += textIn
 }
